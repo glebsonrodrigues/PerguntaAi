8733703c0ee4ca0518f36a86c20c04171c3070c6
Glêbson Rodrigues <glebsonrodrigues@gmail.com>
2024-08-16 19:21:37 -0300

    requeriment.txt

--- perguntaAi/settings.py
+++ perguntaAi/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-%(m9g9l@tpf7mb#msewa6q5l6w@61__-_knf3#)4-654tzyh7z
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
